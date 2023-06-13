9f0c19641e74ebada35dc484d676506aeb6d8efc
JakubPalanis <122232952+JakubPalanis@users.noreply.github.com>
2023-06-13 13:48:04 +0200

    JakubPalaniRecruitmentTask

--- index.php
+++ index.php
@@ -13,6 +13,80 @@
     <?php require_once './partials/main.php'; ?>
 </main>
 
-<script src="assets/js/script.js"></script>
+   <h2>Add User</h2>
+
+   <form method="POST">
+
+    <div class="form-section">
+        <label>ID:</label>
+            <input type="text" name="id" >
+    </div>
+        <br>
+
+    <div class="form-section">
+        <label><b>Name:</b></label>
+        <input type="text" name="name" >
+    </div>
+        <br>
+
+    <div class="form-section">
+        <label><b>Username:</b></label>
+        <input type="text" name="username" >
+    </div>
+        <br>
+
+    <div class="form-section">
+        <label><b>Email:</b></label>
+        <input type="email" name="email" >
+    </div>
+        <br>
+
+    <div class="form-section">
+        <label><b>Address:</b></label>
+        <br>
+        <label>Street:</label>
+        <input type="text" name="street" >
+
+        <label>Suite:</label>
+        <input type="text" name="suite" >
+
+        <label>City:</label>
+        <input type="text" name="city" >
+
+        <label>Zipcode:</label>
+        <input type="text" name="zipcode" >
+    </div>
+        <br>
+    <div class="form-section">
+        <label>Geo:</label><br>
+        <label>Latitude:</label>
+        <input type="text" name="lat">
+
+        <label>Longitude:</label>
+        <input type="text" name="lng" >
+    </div>
+        <br>
+    <div class="form-section">
+        <label><b>Phone:</b></label>   
+        <input type="text" name="phone" ></div><br>
+    <div class="form-section">
+        <label>Website:</label>
+        <input type="text" name="website" >
+    </div>
+        <br>
+    <div class="form-section">
+        <label><b>Company:</b></label>
+        <br>
+
+        <label>Catch Phrase:</label>
+        <input type="text" name="catchPhrase" >
+
+        <label>Business:</label>
+        <input type="text" name="bs" >
+    </div>
+        <br><br>
+        <button type="submit">Add User</button>
+    </form>
+
 </body>
 </html>
--- partials/main.php
+++ partials/main.php
@@ -1,6 +1,83 @@
 <?php
 
-// Please add your logic here
+$json_data = file_get_contents("dataset/users.json");
+$users = json_decode($json_data, true);
 
-echo "<h1 class='starting-title'>Nice to see you! &#128075;</h1>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $newUser = [
+        'id' => $_POST['id'],
+        'name' => $_POST['name'],
+        'username' => $_POST['username'],
+        'email' => $_POST['email'],
+        'address' => [
+            'street' => $_POST['street'],
+            'suite' => $_POST['suite'],
+            'city' => $_POST['city'],
+            'zipcode' => $_POST['zipcode'],
+            'geo' => [
+                'lat' => $_POST['lat'],
+                'lng' => $_POST['lng']
+            ]
+        ],
+        'phone' => $_POST['phone'],
+        'website' => $_POST['website'],
+        'company' => [
+            'name' => $_POST['company'],
+            'catchPhrase' => $_POST['catchPhrase'],
+            'bs' => $_POST['bs']
+        ]
+    ];
 
+    $users[] = $newUser;
+    $json_data = json_encode($users, JSON_PRETTY_PRINT);
+    file_put_contents("dataset/users.json", $json_data);
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+if (isset($_GET['remove'])) {
+    $userId = $_GET['remove'];
+
+    foreach ($users as $key => $user) {
+        if ($user['id'] == $userId) {
+            unset($users[$key]);
+            break;
+        }
+    }
+
+    $json_data = json_encode($users, JSON_PRETTY_PRINT);
+    file_put_contents("dataset/users.json", $json_data);
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+
+if (count($users) != 0) {
+    ?>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Company</th>
+            <th> </th>
+        </tr>
+        <?php foreach ($users as $user) { ?>
+            <tr>
+                <td><?php echo $user['name']; ?></td>
+                <td><?php echo $user['username']; ?></td>
+                <td><?php echo $user['email']; ?></td>
+                <td><?php echo $user['address']['street'] . ', <br />' . $user['address']['city'] . ', <br />' . $user['address']['zipcode']; ?></td>
+                <td><?php echo $user['phone']; ?></td>
+                <td><?php echo $user['company']['name']; ?></td>
+               <td><a href="?remove=<?php echo $user['id']; ?>"><button>Remove</button></a></td>
+            </tr>
+        <?php 
+        } 
+        ?>
+    </table>
+    <?php
+}
+?>
